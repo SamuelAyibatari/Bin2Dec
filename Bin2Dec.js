73ecd2cd73ea40ab448092f555f1dffe414c5f5f
Samuel Ayibatarri <107348130+SamuelAyibatari@users.noreply.github.com>
2023-11-27 15:38:19 +0100

    Update Bin2Dec.js

--- Bin2Dec.js
+++ Bin2Dec.js
@@ -1,1 +1,9 @@
+function binToDec(binary) {
+  if (binary.length > 8 || !/^[01]+$/.test(binary)) {
+    return "Invalid input. Please enter up to 8 binary digits (0 or 1).";
+  }
 
+  return parseInt(binary, 2);
+}
+
+console.log(binToDec(1011111101));
